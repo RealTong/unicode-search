38651577b9004c944ca7cef3ceeb57e9103eb0f2
RealTong <i@wst.sh>
2023-04-24 21:01:23 +0800

    feat: 🎸 First commit
    First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import {Search} from "@/components/Search";
+
+function App() {
+  return (
+    <div className="w-full">
+      <Search/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Search.tsx
@@ -0,0 +1,9 @@
+import {Input} from "@/components/ui/input";
+
+export function Search() {
+  return (
+    <>
+      <Input></Input>
+    </>
+  )
+}
